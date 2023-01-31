9f88db3066bc3959a4e2295fad65900043e7034e
IvanovKS <Skitchi@mail.ru>
2023-01-31 11:51:38 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let str = String(n);
+    let reverse = +str.split('').reverse().join('');    
+    return reverse;
 }
+
+
+// module.exports = function reverse (n) {
+  
+// }
